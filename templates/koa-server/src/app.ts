46be84590ccd476f0066afe9acb3d4df0ce3b21d
shihao wang <shihao.wang@hand-china.com>
2020-08-10 15:39:25 +0800

    feat: 添加compress中间件

--- templates/koa-server/src/app.ts
+++ templates/koa-server/src/app.ts
@@ -3,6 +3,7 @@ import cors from "@koa/cors";
 import { createRootContainer } from "what-di";
 import initRouter from "./routes";
 import DBUtils from "./utils/db.utils";
+import compress from 'koa-compress'
 
 createRootContainer({
   providers: [{ provide: "db", useValue: new DBUtils() }],
@@ -11,6 +12,7 @@ createRootContainer({
 const app = new Koa();
 
 app.use(cors());
+app.use(compress());
 
 initRouter(app);
 
